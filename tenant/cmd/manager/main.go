2cd7d645ed7b14162fe42d16cb9d1bd454ebc93f
Kshitij Gunjikar <kshitij@diamanti.com>
2019-12-17 11:00:14 -0800

    Working , now to test stuff and finish things

--- tenant/cmd/manager/main.go
+++ tenant/cmd/manager/main.go
@@ -22,8 +22,8 @@ import (
 
 	"github.com/diamanticom/multi-tenancy/tenant/pkg/apis"
 	"github.com/diamanticom/multi-tenancy/tenant/pkg/controller"
-	//"github.com/diamanticom/multi-tenancy/tenant/pkg/tenantdb"
-	//"github.com/diamanticom/multi-tenancy/tenant/pkg/webhook"
+	"github.com/diamanticom/multi-tenancy/tenant/pkg/webhook"
+	"github.com/diamanticom/multi-tenancy/tenant/tenantdb"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -69,15 +69,13 @@ func main() {
 		log.Error(err, "unable to register controllers to the manager")
 		os.Exit(1)
 	}
-	/*
-		log.Info("setting up webhooks")
-		if err := webhook.AddToManager(mgr); err != nil {
-			log.Error(err, "unable to register webhooks to the manager")
-			os.Exit(1)
-		}
-	*/
+	log.Info("setting up webhooks")
+	if err := webhook.AddToManager(mgr); err != nil {
+		log.Error(err, "unable to register webhooks to the manager")
+		os.Exit(1)
+	}
 	//Connect to etcd Key Value store
-	//tenantdb.ConnecttoEtcd()
+	tenantdb.ConnecttoBolt()
 
 	// Start the Cmd
 	log.Info("Starting the Cmd.")
